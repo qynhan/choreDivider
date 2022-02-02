2b478bc1e65de1797b7b1b5f20239de77f8c14f4
Jiachun Xiang <jcxiang1230@gmail.com>
2022-02-02 16:05:52 -0500

    decreased "pick your pet" button margin size

--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -1,6 +1,6 @@
 import { Button } from "react-bootstrap"
 import { useUserAuth } from "../contexts/UserAuthContext";
-import Carousel from  "react-elastic-carousel";
+import Carousel from "react-elastic-carousel";
 import Item from "./Item";
 import carousel_img1 from "../image/carousel_img1.png";
 import carousel_img2 from "../image/carousel_img2.png";
@@ -13,17 +13,17 @@ import { ReadMoreBtn } from "./Styledcomponents/StyledComponents";
 
 const useStyles = makeStyles((theme) => ({
     root: {
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "center",
-      alignItems: "center",
-      margin: "20px auto",
-      // margin: "20px"
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        margin: "20px auto",
+        // margin: "20px"
     },
     viewBtn: {
-      position: "relative",
-      top: "160px",
-      margin: "20px auto",
+        position: "relative",
+        top: "160px",
+        margin: "20px auto",
     }
 }));
 
@@ -31,10 +31,10 @@ const useStyles = makeStyles((theme) => ({
 const Home = () => {
     const { user, logOut } = useUserAuth();
     // console.log(user);
-    const handleLogOut = async () =>{
-        try{
+    const handleLogOut = async () => {
+        try {
             await logOut();
-        }catch(err){
+        } catch (err) {
             console.log(err.message);
         }
     }
@@ -43,16 +43,16 @@ const Home = () => {
 
     return (
         <div className="p-4 box my-3 text-center">
-            Hello Welcome <br /> 
-            { user && user.email}
+            Hello Welcome <br />
+            {user && user.email}
 
             <div className={classes.root}>
                 <Wrapper />
                 <CardWrapper />
-                <ReadMoreBtn style={{margin: "100px"}} className={classes.viewBtn}>Pick Your Pet</ReadMoreBtn>
+                <ReadMoreBtn style={{ margin: "72px" }} className={classes.viewBtn}>Pick Your Pet</ReadMoreBtn>
                 {/* <div className="d-grip gap-2 "> */}
                 <div>
-                    <Button variant="primary" onClick={ handleLogOut }>Log out</Button>
+                    <Button variant="primary" onClick={handleLogOut}>Log out</Button>
                 </div>
             </div>
         </div>
