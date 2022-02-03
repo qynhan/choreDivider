e0155115d064750f7a5a2dab00470c27a4274d3a
Jiachun Xiang <jcxiang1230@gmail.com>
2022-02-02 22:19:31 -0500

    added several js and index.css

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -11,13 +11,13 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from "react-router-dom"
 import 'bootstrap/dist/css/bootstrap.min.css'
 import App from "./App"
-
+import './index.css'
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <App />
     </Router>
-    
+
   </React.StrictMode>,
   document.getElementById('root')
 );
