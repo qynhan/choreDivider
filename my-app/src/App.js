3d3ef531cd5535e2e0de4802b683461459e8455b
Jiachun Xiang <jcxiang1230@gmail.com>
2022-02-15 18:15:28 -0500

    add logo.svg

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -12,6 +12,47 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import Card from "./components/GroupsPg/Cards";
 import Task from "./components/Tasks/Task";
 
+import React, { useState, useEffect } from 'react'
+import logo from './logo.svg';
+import './App.css';
+import LoadingScreen from './loading'
+
+
+function App() {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 6000)
+  }, [])
+
+  return (
+    <>
+      {loading === false ? (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      ) : (
+        <LoadingScreen />
+      )}
+    </>
+  );
+}
+
+export default App;
+view rawApp.js hosted with ❤ by GitHub
 function App() {
 
   return (
