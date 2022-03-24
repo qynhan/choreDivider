1deceab9969002ae3cb63e936e4c914e8b200f62
Jiachun Xiang <jcxiang1230@gmail.com>
2022-03-24 17:11:33 -0400

    Update firebase.js

--- my-app/src/firebase.js
+++ my-app/src/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 
-import { initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/combat/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // TODO: Add SDKs for Firebase products that you want to use
